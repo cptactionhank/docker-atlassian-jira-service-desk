c0a044f310537981f0c1cf5b8ce8d6f0107649e0
Martin Aksel Jensen <maage@dotmaage.dk>
2016-08-05 15:46:11 +0200

    Uses GitHub branches instead of CORS protected Docker JS API

--- vendor/js/docker-configurator.js
+++ vendor/js/docker-configurator.js
@@ -12,7 +12,7 @@ app.filter('prefix', function () {
 
 app.run(function($rootScope) {
 
-  // configuration constants for the various Atlassian JIRA Service Desk docker images
+  // configuration constants for the various Atlassian JIRA Software docker images
   // where each key defines the most recent version that had that specific
   // configuration. For example the key 5.8.2 defines the most recent
   // image that supported that configuration and finding a configuration for
@@ -23,11 +23,6 @@ app.run(function($rootScope) {
     // define the default fall-back value of Docker image configuration
     // settings, by using the highest available unicode character.
     '': {
-      home: '/var/local/atlassian/jira',
-      install: '/usr/local/atlassian/jira',
-      java: 7
-    },
-    '6.4.6': {
       home: '/var/atlassian/jira',
       install: '/opt/atlassian/jira',
       java: 8
@@ -38,7 +33,7 @@ app.run(function($rootScope) {
     home: 'not set',
     install: 'not set',
     version: { name:'latest' },
-    java: 'latest',
+    java: 'not set',
     port: 8080
   };
 
@@ -62,11 +57,14 @@ app.controller('ConfigurationController', function($rootScope, $scope, $http) {
   // populate the controllers model with the first 1000 available tags from
   // the Docker Hub repository.
   $http
-    .get('//crossorigin.me/https://hub.docker.com/v2/repositories/cptactionhank/atlassian-jira-service-desk/tags/?page_size=1000')
+    .get('https://api.github.com/repos/cptactionhank/docker-atlassian-jira-service-desk/branches')
     .success(function(data, status) {
-      $scope.tags = data.results.sort(function(a, b) {
+      $scope.tags = data.sort(function(a, b) {
         return String.naturalCompare(a.name, b.name);
+      }).filter(function(item) {
+        return (item.name !== 'gh-pages') && (item.name !== 'master');
       });
+      $scope.tags.push({ name: 'latest' });
       // set the latest tag as the default selected
       $rootScope.jira.version = $scope.tags.filter(function(item) {
         return item.name === 'latest';
